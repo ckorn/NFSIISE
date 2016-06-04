ckorn/NFSIISE

commit cfb3ae640adef572e583181be75d8d87e015a485

Bugfix: SDL mutexes are recursive, so it can't lock the same thread
Use volatile variable and loop instead.
Refactor the code (WIP).

src/Wrapper.c
@@ -254,9 +254,15 @@ uint16_t PORT1 = 1030, PORT2 = 1029;
 
 REALIGN void WrapperInit(void)
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING & ~SDL_INIT_GAMECONTROLLER) < 0)
+		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+
 	BOOL useOnlyOneCPU = true;
 	uint32_t msaa = 0;
 	FILE *f = NULL;
+
+	SDL_JoystickEventState(SDL_IGNORE);
+	SDL_ShowCursor(false);
 
 #ifdef WIN32
 	const char *homeDir = getenv("AppData");
@@ -590,27 +596,11 @@ REALIGN time_t time_wrap(time_t *timer)
 
 /* WIP code for swap main_thread<->window_thread for Cocoa(OSX) */
 
-static SDL_mutex *win_mutex;
+extern volatile uint8_t canRunWindowThread;
 
-void mainCodeInSeparateThread();
-
-REALIGN void startInThread()
+REALIGN STDCALL void startInThread(SDL_ThreadFunction mainCodeInSeparateThread)
 {
-	if (SDL_Init((SDL_INIT_EVERYTHING | SDL_INIT_NOPARACHUTE) & ~SDL_INIT_GAMECONTROLLER) < 0)
-		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
-
-	SDL_JoystickEventState(SDL_IGNORE);
-	SDL_ShowCursor(false);
-
-	win_mutex = SDL_CreateMutex();
-	SDL_LockMutex(win_mutex);
-
-	SDL_DetachThread(SDL_CreateThread((SDL_ThreadFunction)mainCodeInSeparateThread, NULL, NULL));
-
-	SDL_LockMutex(win_mutex); //Wait...
-}
-
-REALIGN void windowThread()
-{
-	SDL_UnlockMutex(win_mutex);
+	SDL_DetachThread(SDL_CreateThread(mainCodeInSeparateThread, NULL, NULL));
+	while (!canRunWindowThread)
+		SDL_Delay(1);
 }

src/NFS2SE.asm
@@ -10,6 +10,7 @@ global dword_4DDA70
 global dword_5637A0
 global mousePositionX
 global mousePositionY
+global canRunWindowThread
 
 extern SDL_NumJoysticks_wrap
 extern SDL_GetTicks_wrap
@@ -71,6 +72,7 @@ extern guFogGenerateExp
 extern WrapperCreateWindow
 extern fetchTrackRecords
 extern WrapperAtExit
+extern startInThread
 extern WrapperInit
 extern startTimer
 extern stopTimer
@@ -169879,8 +169881,7 @@ loc_480EDC:
 ; 	call sub_489AE0
 ; 	test eax, eax
 ; 	jz loc_480F63
-extern windowThread
-	call windowThread
+	mov byte [canRunWindowThread], 1
 
 	xor edx, edx
 
@@ -218415,15 +218416,19 @@ loc_4B2D47:
 	ret 4
 ;sub_4B2D3C endp
 
-global mainCodeInSeparateThread
-extern startInThread
-
 ;	Attributes: library function bp-based frame
 
 start: ;SUBROUTINE
+	push mainCodeInSeparateThread
 	call startInThread
 	call sub_481080 ;Call the window thread function, but do it in main thread
-	jmp $ ;Infinity loop (TODO: lock and wait for exit)
+
+infinitySleep:
+	push 1 ; bAlertable
+	push 0FFFFFFFFh ; dwMilliseconds
+	call SleepEx_wrap
+	jmp infinitySleep
+;start endp
 
 mainCodeInSeparateThread:
 	call WrapperInit
@@ -218441,7 +218446,7 @@ mainCodeInSeparateThread:
 	mov [ecx+0F0h], eax
 	mov dword [lpTlsValue], ecx
 	jmp sub_4BB81E
-;start endp
+;mainCodeInSeparateThread endp
 
 sub_4B2FD0: ;SUBROUTINE
 	push edx
@@ -238700,6 +238705,7 @@ dword_4E2AEC: dd 2000h
 	dd 2AA1h, 2D41h, 3249h, 3B21h, 4B42h, 6D41h, 0D650h, 73FCh
 	dd 539Fh, 58C5h, 62A3h, 73FCh, 939Fh, 0D650h, 1A463h, 0
 timerIsRunning: dd 0
+canRunWindowThread: db 0
 
 section .bss
 

commit 981f18401e50155228b104af9e4a819a79fc7214

Don't initialize SDL2 in separate thread, it breaks OS X support
Regression after cfb3ae640adef572e583181be75d8d87e015a485

src/Wrapper.c
@@ -254,9 +254,6 @@ uint16_t PORT1 = 1030, PORT2 = 1029;
 
 REALIGN void WrapperInit(void)
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING & ~SDL_INIT_GAMECONTROLLER) < 0)
-		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
-
 	BOOL useOnlyOneCPU = true;
 	uint32_t msaa = 0;
 	FILE *f = NULL;
@@ -600,6 +597,8 @@ extern volatile uint8_t canRunWindowThread;
 
 REALIGN STDCALL void startInThread(SDL_ThreadFunction mainCodeInSeparateThread)
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING & ~SDL_INIT_GAMECONTROLLER) < 0)
+		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
 	SDL_DetachThread(SDL_CreateThread(mainCodeInSeparateThread, NULL, NULL));
 	while (!canRunWindowThread)
 		SDL_Delay(1);